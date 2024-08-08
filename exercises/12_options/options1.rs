7d411c65684824d35b9e71ddf1de5afb3e1748d3
banber0 <lzx0975@gmail.com>
2024-08-08 11:22:20 +0800

    o

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -5,11 +5,11 @@
 
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
-match hour_of_day {
-    0..=21 => Some(5),
-    22..=23 => Some(0),
-    _ => None,
-}
+    match hour_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None,
+    }
 }
 
 fn main() {
@@ -24,7 +24,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
